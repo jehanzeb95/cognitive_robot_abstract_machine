ee6bebd26bf6282d9117044848cb41be41a47f33
Tom Schierenbeck <tom_sch@uni-bremen.de>
2025-12-16 15:38:01 +0100

    [ORMatic] Added method to compute inheritance path length.

--- krrood/src/krrood/ormatic/utils.py
+++ krrood/src/krrood/ormatic/utils.py
@@ -6,6 +6,7 @@ import json
 import types
 from contextlib import suppress
 from enum import Enum
+from functools import lru_cache
 from types import ModuleType
 
 import sqlalchemy
@@ -27,6 +28,7 @@ from typing_extensions import (
     Tuple,
     Dict,
     Any,
+    Optional,
 )
 
 from .dao import AlternativeMapping, DataAccessObject
@@ -203,3 +205,42 @@ def create_engine(url: Union[str, URL], **kwargs: Any) -> Engine:
         json_deserializer=lambda x: from_json(json.loads(x)),
         **kwargs,
     )
+
+
+@lru_cache
+def inheritance_path_length(child_class: Type, parent_class: Type) -> Optional[int]:
+    """
+    Calculate the inheritance path length between two classes.
+    Every inheritance level that lies between `child_class` and `parent_class` increases the length by one.
+    In case of multiple inheritance, the path length is calculated for each branch and the minimum is returned.
+
+    :param child_class: The child class.
+    :param parent_class: The parent class.
+    :return: The minimum path length between `child_class` and `parent_class` or None if no path exists.
+    """
+    if not issubclass(child_class, parent_class):
+        return None
+
+    return _inheritance_path_length(child_class, parent_class, 0)
+
+
+def _inheritance_path_length(
+    child_class: Type, parent_class: Type, current_length: int = 0
+) -> int:
+    """
+    Helper function for :func:`inheritance_path_length`.
+
+    :param child_class: The child class.
+    :param parent_class: The parent class.
+    :param current_length: The current length of the inheritance path.
+    :return: The minimum path length between `child_class` and `parent_class`.
+    """
+
+    if child_class == parent_class:
+        return current_length
+    else:
+        return min(
+            _inheritance_path_length(base, parent_class, current_length + 1)
+            for base in child_class.__bases__
+            if issubclass(base, parent_class)
+        )
