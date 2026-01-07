214553cdb6da12dd90f99cfd3d4c89013999ceb3
Jonas Dech <j.dech@gmx.net>
2026-01-07 10:14:56 +0100

    Alternative mapping for MoveTCP

--- pycram/src/pycram/alternative_motion_mappings/hsrb_motion_mapping.py
+++ pycram/src/pycram/alternative_motion_mappings/hsrb_motion_mapping.py
@@ -1,12 +1,14 @@
 from nav2_msgs.action import NavigateToPose
 
+from giskardpy.motion_statechart.tasks.cartesian_tasks import CartesianPose
 from giskardpy.motion_statechart.tasks.ros_tasks import (
     ActionServerTask,
     NavigateActionServerTask,
 )
 from semantic_digital_twin.robots.hsrb import HSRB
 from ..datastructures.enums import ExecutionType
-from ..robot_plans import MoveMotion, MoveGripperMotion
+from ..robot_description import ViewManager
+from ..robot_plans import MoveMotion, MoveGripperMotion, MoveTCPMotion
 
 from ..robot_plans.motions.base import AlternativeMotion
 
@@ -25,4 +27,38 @@ class HSRBMoveMotion(MoveMotion, AlternativeMotion[HSRB]):
             action_topic="/hsrb/move_base",
             node_handle=self.plan.context.ros_node,
             message_type=NavigateToPose,
+        )
+
+
+class HSRMoveTCPSim(MoveTCPMotion, AlternativeMotion[HSRB]):
+
+    execution_type = ExecutionType.SIMULATED
+
+    def perform(self):
+        return
+
+    @property
+    def _motion_chart(self) -> CartesianPose:
+        tip = ViewManager().get_end_effector_view(self.arm, self.robot_view).tool_frame
+        return CartesianPose(
+            root_link=self.world.root,
+            tip_link=tip,
+            goal_pose=self.target.to_spatial_type(),
+        )
+
+
+class HSRMoveTCPReal(MoveTCPMotion, AlternativeMotion[HSRB]):
+
+    execution_type = ExecutionType.REAL
+
+    def perform(self):
+        return
+
+    @property
+    def _motion_chart(self) -> CartesianPose:
+        tip = ViewManager().get_end_effector_view(self.arm, self.robot_view).tool_frame
+        return CartesianPose(
+            root_link=self.world.root,
+            tip_link=tip,
+            goal_pose=self.target.to_spatial_type(),
         )
